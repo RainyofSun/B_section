a2de1dcf358510b7d64d29cabf4de2bc310446a5
EGLS_BMAC <egls_bmac@EGLSMAC.local>
2020-11-05 18:39:56 +0800

    B Module 模块配置

---
+++ B_section/B_section/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  B_section
+//
+//  Created by EGLS_BMAC on 2020/11/5.
+//  Copyright © 2020 EGLS_BMAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ B_section/B_section/AppDelegate.m
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.m
+//  B_section
+//
+//  Created by EGLS_BMAC on 2020/11/5.
+//  Copyright © 2020 EGLS_BMAC. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "BViewController.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    [self rootVC];
+    return YES;
+}
+
+- (void)rootVC {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[BViewController alloc] init]];
+}
+
+
+@end
